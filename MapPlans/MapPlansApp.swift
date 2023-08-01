4597f0a7ce9306f7a510a5f77c22ff259bb6270c
VanyaTobiasFate <i.kucherenko@outloud.games>
2023-08-01 13:57:02 +0300

    Initial Commit

---
+++ MapPlans/MapPlansApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapPlansApp.swift
+//  MapPlans
+//
+//  Created by Tobias on 01.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct MapPlansApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
